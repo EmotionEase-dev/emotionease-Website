21205fb9b9bd5ade0609fbbafea0db81418c9c6a
EmotionEase-dev <emotionease@gmail.com>
2025-05-14 22:21:20 +0530

    localhost update

--- pages/ContactUs.jsx
+++ pages/ContactUs.jsx
@@ -22,7 +22,7 @@ const ContactUs = () => {
 
   // Create Axios instance with base URL
   const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://backend-xhbf.onrender.com/api',
     headers: {
       'Content-Type': 'application/json'
     }
