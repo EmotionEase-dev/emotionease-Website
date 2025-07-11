2179740478eddca6793b4c4a07e73d7601547bff
ThilagaMC <thilaga2k2@gmail.com>
2025-07-11 20:42:28 +0530

    sitemap.xml

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,6 @@ import About from '../pages/About';
 import ContactUs from '../pages/ContactUs';
 import CuratedPrograms from '../pages/CuratedPrograms';
 import PrivacyPolicy from '../components/PrivacyPolicy';
-import sitemap from '../sitemap.xml';
-
 import WorkplaceStressManagementPage from '../pages/WorkplaceStress';
 import StressManagementPage from '../pages/StressManagement';
 import ParentCoachingPage from '../pages/ParentCoaching';
@@ -123,8 +121,6 @@ function App() {
         <Route path="/BurnoutTherapy" element={<BurnoutTherapyPage />} />
         <Route path="/StressManagement" element={<StressManagementPage />} />
         <Route path="/CoupleTherapy" element={<CouplesTherapyPage />} />
-        {/* Sitemap route */}
-        <Route path="/home/sitemap.xml" element={<div dangerouslySetInnerHTML={{ __html: sitemap }} />} />
 
         {/* Catch-all route for 404 Not Found */}
         <Route path="/404" element={<div className="not-found">404 Not Found</div>} />
