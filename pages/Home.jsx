7244fb996201b46d452558f66fa8ed97f3e42edc
EmotionEase <emotionease@gmail.com>
2025-08-19 18:03:31 +0530

    Update Home.jsx

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1347,26 +1347,6 @@ const Home = () => {
                     name: "linkedin",
                     url: "https://www.linkedin.com/company/emotionease/",
                   },
-                  {
-                    <style>
-                      .libutton {
-                      display: flex;
-                      flex-direction: column;
-                      justify-content: center;
-                      padding: 7px;
-                      text-align: center;
-                      outline: none;
-                      text-decoration: none !important;
-                      color: #ffffff !important;
-                      width: 200px;
-                      height: 32px;
-                      border-radius: 16px;
-                      background-color: #0A66C2;
-                      font-family: "SF Pro Text", Helvetica, sans-serif;
-                              }
-                    </style>
-                  <a className="libutton" href="https://www.linkedin.com/build-relation/newsletter-follow?entityUrn=7350806259647885314" target="_blank">Subscribe on LinkedIn</a>,
-                  },
                   // { name: 'youtube', url: 'https://www.youtube.com/@emotionease' }
                 ].map((social, index) => (
                   <motion.a
@@ -1495,6 +1475,7 @@ const Home = () => {
 
 
 export default Home;
+
 
 
 
